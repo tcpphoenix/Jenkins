0b587d73141039fb6da734d7f5265dd54d1a4c05
rezo <rezo@example.com>
2021-07-17 16:39:02 +0000

    This is a new commit for what I originally planned to be amended

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in terminal window.
+        System.out.println("Hello, World");
+    }
+}
+
